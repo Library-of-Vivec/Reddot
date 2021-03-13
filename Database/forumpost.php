435a0c7e488e0e278a877d668162828a7fe3c589
AshenAwakener <201811468@feualabang.edu.ph>
2021-03-13 20:25:50 +0800

    Update forumpost.php

--- Database/forumpost.php
+++ Database/forumpost.php
@@ -102,32 +102,76 @@
 		echo "</br>Profile Pic: <img src=\"".$row["profilepic"]."\" height=50 width=50>";
 	    }
 	}
-	//DISPLAY USERNAME AND PROFILE PIC
+
+?>
+
+<?php
     $title = $_GET['title'];
-    $sql = "SELECT title, post FROM forums WHERE title = '$title'";
+    include 'config.php';
+    $sql = "SELECT title, post ,email_user FROM forum WHERE title = '$title'";
     $result = mysqli_query($conn, $sql);
-    while($row = mysqli_fetch_assoc($result)){
+	    while($row = mysqli_fetch_assoc($result)){
       foreach($row as $key => $value){
-        echo $row["title"]."<br>";
+		echo "<h2 style='color:green'> Posted BY: ".$row['email_user']."</h2><br>";
+        echo "<h2> Title: ".$row["title"]."</h2><br>";
         echo $row["post"];
-        echo "<br>";
+        echo "<br><br><br>";
         break;
       }
     }
+?>
 
-    $sql = "SELECT comment FROM comment WHERE title = '$title'";
+<?php
+    $sql = "SELECT likes FROM forum WHERE title = '$title'";
+    $like = mysqli_query($conn, $sql);
+	$numlikes = mysqli_fetch_assoc($like);
+	$sql = "SELECT username from account WHERE email='".$email."'";
+	$unamesql = mysqli_query($conn, $sql);
+	$unameres = mysqli_fetch_assoc($unamesql);
+	echo "<h4>Number of helpfulness: ".$numlikes['likes']."</h4>";
+	$uname = $unameres['username'];
+	$sql = "SELECT * FROM likes WHERE username = '$uname' AND title = '$title'";
+	$likes = mysqli_query($conn, $sql);
+?>
+
+<form method="POST">
+    <input type="submit" name="like" value="Helpful"/>
+</form>
+
+<?php
+	if(mysqli_num_rows($likes) != 0) {
+		echo "You find this post as helpful.";
+	}
+	else {
+		if(isset($_POST['like'])){
+			$sql = "UPDATE forum SET likes = likes + 1 WHERE title = '$title'";
+			$likes = mysqli_query($conn, $sql);
+			$sql = "INSERT INTO likes(username, title)
+                        VALUES('$uname', '$title')";
+			$likes = mysqli_query($conn, $sql);
+			echo "<meta http-equiv='refresh' content='0'>";
+		}
+	}
+?>
+
+<?php
+    $sql = "SELECT username, comment FROM comment WHERE title = '$title'";
     $result = mysqli_query($conn, $sql);
+	echo "<h3>Comments section: </h3>";
     while($row = mysqli_fetch_assoc($result)){
       foreach($row as $key => $value){
+		echo $row['username'].": ";
         echo $row["comment"]."<br>";
         break;
       }
     }
+?>
 
+<?php
   if(isset($_POST['submit'])){
     $comment = $_POST['comment'];
-    $sql = "INSERT INTO comment(title, comment)
-                        VALUES('$title', '$comment')";
+    $sql = "INSERT INTO comment(title, username, comment)
+                        VALUES('$title', '$uname', '$comment')";
     $insert = mysqli_query($conn,$sql);
 
     if($insert){
@@ -139,7 +183,6 @@
   }
 ?>
  
-
 <form action="" method="post">
   <textarea name="comment" placeholder="Comment here" rows="5" cols="100" style="resize:none"></textarea><br>
   <input type="submit" name="submit" value="Submit"></input>
