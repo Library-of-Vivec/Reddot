1558056e6625f538cf11ca640d198b97d19a11ab
armaroz <jensjovellano@gmail.com>
2021-03-16 20:47:05 +0800

    Update forumdisp.php

--- Database/forumdisp.php
+++ Database/forumdisp.php
@@ -24,7 +24,7 @@
   }
     body {
       font-family: Arial, Helvetica, sans-serif;
-    background-image: url('bg.jpg');
+    background-image: url('reddot_bg2.jpg');
     background-repeat: no-repeat;
     background-attachment: fixed;
     }
@@ -71,17 +71,18 @@
       display: block;
     }
   .navbar{
-    height:5em;
+    height:6.2em;
     width: auto;
     margin: 0 auto;
     text-align: right;
     position: sticky;
     top:0;
-    background-color: rgba(255, 255, 255, 0.7);
+    background-color: rgba(255, 255, 255, 1);
+    box-shadow:0px 2px 5px grey;
   }
   button{
     transition-duration: 0.5s;
-    background-color:grey;
+    background-color:white;
     border-style: none;
     padding: 15px 32px;
   }
@@ -107,21 +108,117 @@
     background-color:transparent;
     background-repeat: no-repeat;
     border: none;
-    border-radius:15px;
     cursor:pointer;
     overflow: hidden;
     outline:none;
     background-repeat:no-repeat;
     height:75px;
-    width:150px;
+    width:75px;
     -webkit-transition-duration:0.5s;
   }
+  .btnHead:hover{
+  }
   .logo{
+    float:left;
     height: 75px;
     width: 75px;
     display: inline-block;
-    margin-left: auto;
-    margin-right: 45em;
+    margin-left: 30px;
+    margin-top: 10px;
+  }
+  .ppic{
+    border-radius: 35px;
+  }
+  .searchbtn{
+    margin-top: 9px;
+    margin-left: 10px;
+    background:url(search.png) 5px 10px no-repeat;
+    height:3.3em;
+    width:10px;
+    float:left;
+  }
+  .searchbtn:disabled:hover{
+    margin-left: 10px;
+    float:left;
+    background:url(search.png) 5px 10px no-repeat;
+    cursor:not-allowed;
+  }
+  .searchbtn:hover{
+    margin-left: 10px;
+    float:left;
+    background:url(search3.png) 5px 10px no-repeat;
+    cursor:pointer;
+  }
+  .searchbox{
+    border-top:none;
+    border-left:none;
+    border-right:none;
+    float:left;
+    margin-top:30px;
+    margin-left:325px;
+    height:2em;
+    width:50em;
+    font-size:15pt;
+  }
+  .searchbox:focus{
+    outline:none;
+    border-left:none;
+    border-top:none;
+    border-right:none;
+    float:left;
+    margin-top:30px;
+    margin-left:325px;
+    height:2em;
+    width:50em;
+    font-size:15pt;
+  }
+  .dispdiv{
+    background-color:rgb(59,59,59);
+    margin-top:25px;
+    margin-left:15%;
+    float:left;
+    border-width: 1px;
+    border-style:solid;
+    padding:7px;
+    border-radius: 5px;
+    width:35%;
+    height:auto;
+  }
+  .dispdiv:hover{
+    transition-duration: 0.5s;
+    background-color:rgb(219,217,217);
+    margin-top:25px;
+    margin-left:15%;
+    float:left;
+    border-width: 1px;
+    border-style:solid;
+    padding:7px;
+    border-radius: 5px;
+    width:35%;
+    height:auto;
+  }
+  .createpost{
+    border-style: hidden;
+    border-radius: 6px;
+    padding-left:10px;
+    padding-top:15px;
+    text-align: left;
+    background-color:rgb(130,130,130);
+    height:50px;
+    width:100%;
+    font-size:12pt;
+  }
+  .createpost:hover{
+    border-style: hidden;
+    border-radius: 6px;
+    background-color:rgb(59,59,59);
+    padding-left:10px;
+    padding-top:15px;
+    text-align: left;
+    height:50px;
+    width:100%;
+    cursor:text;
+    font-size:12pt;
   }
 </style>
   </head>
@@ -145,12 +242,9 @@
         }
     }
     echo "<div class=\"navbar\">
-    <button class = \"btnHead\"><a href=\"landingpage.php\"><img src=\"home.png\" height=50 width=50></a></button>
-    <button class = \"btnHead\"><a href=\"forumtest.php\"><img src=\"create.png\" height=50 width=50></a></button>
-    <button class = \"btnHead\"><a href=\"display_all.php\"><img src=\"likes.png\" height=50 width=50></a></button>
-    <img class = \"logo\" src = \"RED_DOT_1.png\">
+    <a href=\"forumdisp.php\"><img class = \"logo\"src=\"RED_DOT_1.png\" height=60 width=60></a>
     <div class=\"dropdown\">
-    <button class=\"profile\"><img src='$profpic' width=30 height=30>&nbsp;&nbsp;$username
+    <button class=\"profile\"><img class = \"ppic\" src='$profpic' width=65 height=65>
       <i class=\"fa fa-caret-down\"></i>
       </button>
       <div class=\"dropdown-content\">
@@ -159,14 +253,27 @@
         <a href='' onclick =\"signOut()\">Sign Out</a>
         </div>
     </div>";
+    echo "
+    <div class=\"search-container\">
+    <form action=\"\" method=\"post\" id=\"searchform\">
+      <input rows = '5' type=\"text\" name=\"search_field\" placeholder=\"Search...\" id=\"search_field\" class='searchbox'></input><br>
+      <button type=\"submit\" name=\"search\" value=\"Search\" id=\"searchbtn\" disabled=\"disabled\" class='searchbtn'></button>
+    </form>
+    </div>";
     echo "</div>";
     //DISPLAY USERNAME AND PROFILE PIC
-	//DISPLAY USERNAME AND PROFILE PIC
+  //CREATE POST BUTTON
+  ?>
 
-  $sql = "SELECT title, email_user, post, id, likes FROM forum";
+  <div class="dispdiv"><button class = 'createpost'><a href="forumtest.php" style="text-decoration:none; cursor:text; color:black;">Create Post...</a></button></div>
+  <?php
+	if(isset($_POST['search'])){
+		$search = $_POST['search_field'];
+		header("location:searchpost.php?search_post=$search");
+	}
+	 $sql = "SELECT title, email_user, post, id, likes FROM forum";
   $result = mysqli_query($conn, $sql);
   if($email == "201811471@feualabang.edu.ph" || $email == "201810285@feualabang.edu.ph" || $email == "201811597@feualabang.edu.ph" || $email == "201811285@feualabang.edu.ph"){
-    echo "admin page<br>";
     echo "<form action = '' method = 'post'>";
     while($row = mysqli_fetch_assoc($result)){
 		echo "<div class=\"posts\">";
@@ -197,12 +304,12 @@
     echo "</form>";
   }
   else{
-    echo "user page <br>";
-    while($row = mysqli_fetch_assoc($result)){
 
+    while($row = mysqli_fetch_assoc($result)){
+      echo "<div class = 'dispdiv'>";
 		echo "<div class=\"posts\">";
 		$value = $row["id"];
-        echo "<a href='forumpost.php?post_id=$value'>".$row["title"]."</a><br>";
+    echo "<a href='forumpost.php?post_id=$value'>".$row["title"]."</a><br>";
 		if(strlen($row["post"]) > 198){
 			echo "<p>".substr($row["post"], 0,198)."...</p>";
 		}
@@ -222,8 +329,21 @@
 		while($row1 = mysqli_fetch_assoc($get_user_r)){
 			echo "<p>posted by: ".$row1["username"]."</p></div>";
 		}
-
+    echo "</div>";
     }
+  }
+
+  if(isset($_POST['delsub'])){
+    foreach($_POST['checkdelete'] as $selected) {
+      echo $selected;
+        $sqldel = "DELETE FROM forum WHERE id = '$selected'";
+        $delres = mysqli_query($conn,$sqldel);
+		$sqldel2 = "DELETE FROM likes WHERE post_id = '$selected'";
+		$delres2 = mysqli_query($conn,$sqldel2);
+		$sqldel3 = "DELETE FROM comment WHERE post_id = '$selected'";
+		$delres3 = mysqli_query($conn,$sqldel3);
+}
+	echo "<meta http-equiv='refresh' content = '0'>";
   }
 
   if(isset($_POST['delsub'])){
@@ -264,7 +384,14 @@
 
 	}
 	///////////////////////////////////////////////
-
+  searchform.addEventListener('input', () => {
+        if(search_field.value != ''){
+            searchbtn.removeAttribute('disabled');
+        }
+        else{
+            searchbtn.setAttribute('disabled', 'disabled');
+        }
+    });
   </script>
  </body>
 </html>
