975625e1179f35d59e4d3b0e446334a61ef5227f
armaroz <jensjovellano@gmail.com>
2021-03-18 21:30:20 +0800

    Update editprofile.php

--- Database/editprofile.php
+++ Database/editprofile.php
@@ -18,13 +18,18 @@
     transform: scaleY(1)
   }
 }
+@font-face{
+  font-family:fontnikelsey;
+  src: url('space-mono.ttf');
+}
   *{
   padding: 0px;
   margin: 0px;
+  font-family:fontnikelsey;
   }
     body {
       font-family: Arial, Helvetica, sans-serif;
-    background-image: url('bg.jpg');
+    background-image: url('pages_bg.jpg');
     background-repeat: no-repeat;
     background-attachment: fixed;
     }
@@ -51,6 +56,7 @@
     }
 
     .dropdown-content a {
+      background-color: rgba(44,55,57,1);
     transition-duration: 0.5s;
       float: none;
       color: black;
@@ -71,17 +77,18 @@
       display: block;
     }
   .navbar{
-    height:5em;
+    height:6.2em;
     width: auto;
     margin: 0 auto;
     text-align: right;
     position: sticky;
     top:0;
-    background-color: rgba(255, 255, 255, 0.7);
+    background-color: rgba(44, 53, 57, 1);
+    box-shadow:0px 2px 5px grey;
   }
   button{
     transition-duration: 0.5s;
-    background-color:grey;
+    background-color:white;
     border-style: none;
     padding: 15px 32px;
   }
@@ -92,9 +99,19 @@
     padding: 15px 32px;
   }
   .profile{
+    background-color: rgba(0,0,0,0);
     height: 100%;
     width:160px;
     border-style: none;
+    padding: 15px 32px;
+    display: inline-block;
+    margin-right: 10%;
+  }
+  .profile:hover{
+    background-color: rgba(0,0,0,0);
+    height: 100%;
+    width:160px;
+    border-style: solid;
     padding: 15px 32px;
     display: inline-block;
     margin-right: 10%;
@@ -107,22 +124,208 @@
     background-color:transparent;
     background-repeat: no-repeat;
     border: none;
-    border-radius:15px;
     cursor:pointer;
     overflow: hidden;
     outline:none;
     background-repeat:no-repeat;
     height:75px;
-    width:150px;
+    width:75px;
     -webkit-transition-duration:0.5s;
   }
   .logo{
+    float:left;
     height: 75px;
     width: 75px;
     display: inline-block;
-    margin-left: auto;
-    margin-right: 45em;
+    margin-left: 30px;
+    margin-top: 10px;
   }
+  .ppic{
+    border-radius: 35px;
+  }
+  .searchbtn{
+    margin-top: 9px;
+    margin-left: 10px;
+    background:url(search.png) 5px 10px no-repeat;
+    height:3.3em;
+    width:10px;
+    float:left;
+  }
+  .searchbtn:disabled:hover{
+    margin-left: 10px;
+    float:left;
+    background:url(search.png) 5px 10px no-repeat;
+    cursor:not-allowed;
+  }
+  .searchbtn:hover{
+    margin-left: 10px;
+    float:left;
+    background:url(search3.png) 5px 10px no-repeat;
+    cursor:pointer;
+  }
+  .searchbox{
+    background-color: rgba(0,0,0,0);
+    border-top:none;
+    border-left:none;
+    border-right:none;
+    float:left;
+    margin-top:30px;
+    margin-left:325px;
+    height:2em;
+    width:50em;
+    font-size:15pt;
+  }
+  .searchbox:focus{
+    outline:none;
+    border-left:none;
+    border-top:none;
+    border-right:none;
+    float:left;
+    margin-top:30px;
+    margin-left:325px;
+    height:2em;
+    width:50em;
+    font-size:15pt;
+  }
+  .dispdiv{
+    transition-duration: 0.5s;
+    background-color:rgb(130,130,130);
+    margin-top:25px;
+    margin-left:15%;
+    float:left;
+    border-width: 1px;
+    border-style:solid;
+    padding:7px;
+    border-radius: 5px;
+    width:35%;
+    height:auto;
+    color:rgb(182,182,180);
+  }
+  .dispdivcreate{
+    border-color:white;
+    background-color:rgb(59,59,59);
+    margin-top:0px;
+    margin-left:15%;
+    margin-bottom:25px;
+    float:left;
+    border-width: 1px;
+    border-style:solid;
+    padding:7px;
+    border-radius: 5px;
+    width:35%;
+    height:auto;
+  }
+  .dispdiv:hover{
+    transition-duration: 0.5s;
+    background-color:rgb(59,59,59);
+    margin-top:25px;
+    margin-left:15%;
+    float:left;
+    border-width: 1px;
+    border-style:solid;
+    padding:7px;
+    border-radius: 5px;
+    width:35%;
+    height:auto;
+  }
+  .createpost{
+    transition-duration: 0s;
+    border-style: hidden;
+    border-radius: 6px;
+    padding-left:10px;
+    padding-top:15px;
+    text-align: left;
+    background-color:rgb(130,130,130);
+    height:50px;
+    width:100%;
+    font-size:12pt;
+  }
+  .createpost:hover{
+    transition-duration: 0s;
+    border-style: solid;
+    border-color: black;
+    border-radius: 6px;
+    background-color:rgb(59,59,59);
+    padding-left:10px;
+    padding-top:15px;
+    text-align: left;
+    height:50px;
+    width:100%;
+    cursor:text;
+    font-size:12pt;
+  }
+  .rightitems{
+    margin-bottom:20px;
+    background-color:white;
+    width:auto;
+    margin-bottom:10px;
+    font-size:15pt;
+    background-color:rgb(59,59,59);
+    color:rgb(182,182,180);
+    border-bottom: solid 1px white;
+    padding:5px;
+
+  }
+  .rightcontainer{
+    background-color:rgb(59,59,59);
+    overflow-x: hidden;
+    overflow-y: auto;
+    margin-top:50px;
+    margin-left:1000px;
+    border-width: 0.5px;
+    border-style:solid;
+    border-radius: 5px;
+    width:35%;
+    height:auto;
+    border-color: white;
+
+  }
+  a{
+    font-weight: bold;
+    text-decoration: none;
+    color:white;
+  }
+  input[type="submit"]{
+    font-size:13pt;
+    width:auto;
+    color: black;
+    text-align: center;
+    background-color:rgb(130,130,130);
+    border: none;
+    padding:5px;
+    border-radius: 4px;
+}
+.boxcontainer{
+  background-color:rgba(255,255,255,0.5);
+  margin:auto;
+  margin-top: 100px;
+  border-width: 1px;
+  border-style:solid;
+  padding:7px;
+  border-radius: 5px;
+  width:50%;
+}
+.textareas::-webkit-input-placeholder{
+ text-align: center;
+ color:white;
+}
+.textareas{
+  width: 100%;
+  padding:10px;
+  resize: none;
+  background-color:rgb(59,59,59);
+  color:white;
+}
+.submitpost{
+  transition-duration: 0.5s;
+  margin-left:auto;
+  margin-right:auto;
+  display:block;
+}
+.submitpost:hover{
+  transition-duration: 0.5s;
+  background-color:rgb(87,87,87);
+}
 </style>
   </head>
   <body>
@@ -144,21 +347,26 @@
 	    }
 	}
   echo "<div class=\"navbar\">
-  <button class = \"btnHead\"><a href=\"landingpage.php\"><img src=\"home.png\" height=50 width=50></a></button>
-  <button class = \"btnHead\"><a href=\"forumtest.php\"><img src=\"create.png\" height=50 width=50></a></button>
-  <button class = \"btnHead\"><a href=\"display_all.php\"><img src=\"likes.png\" height=50 width=50></a></button>
-  <img class = \"logo\" src = \"RED_DOT.png\">
-	<div class=\"dropdown\">
-	<button class=\"profile\"><img src='$profpic' width=30 height=30>&nbsp;&nbsp;$username
-	  <i class=\"fa fa-caret-down\"></i>
-	  </button>
-	  <div class=\"dropdown-content\">
-      <a href=\"profile.php\">Profile</a>
+  <a href=\"landingpage.php\"><img class = \"logo\"src=\"RED_DOT_1.png\" height=60 width=60></a>
+  <div class=\"dropdown\">
+  <button class=\"profile\"><img class = \"ppic\" src='$profpic' width=65 height=65>
+    <i class=\"fa fa-caret-down\"></i>
+    </button>
+    <div class=\"dropdown-content\">
+      <a href='profile.php?user=$username'>Profile</a>
       <a href=\"editprofile.php\">Edit Profile</a>
-      <a href='' onclick =\"signOut()\">Sign Out</a>
+      <a href=\"display_all.php\">Liked Posts</a>
+      <a onclick =\"signOut()\">Sign Out</a>
       </div>
-	</div>";
-	echo "</div>";
+  </div>";
+  echo "
+  <div class=\"search-container\">
+  <form action=\"\" method=\"post\" id=\"searchform\">
+    <input rows = '5' type=\"text\" name=\"search_field\" placeholder=\"Search...\" id=\"search_field\" class='searchbox'></input><br>
+    <button type=\"submit\" name=\"search\" value=\"Search\" id=\"searchbtn\" disabled=\"disabled\" class='searchbtn'></button>
+  </form>
+  </div>";
+  echo "</div>";
 	//DISPLAY USERNAME AND PROFILE PIC
   ?>
 
@@ -169,7 +377,6 @@
 
   <?php
     $email = $_COOKIE["email"];
-	echo $email;
 	$pos = strpos($email,"@");
 	$id = substr($email, 0, $pos);
 	//INSERT AND INITIALIZE VALUES
@@ -181,11 +388,16 @@
 	  //GET DEFAULT PICTURE
 	  //RENAMING DEFAULT PICTURE TO STUDENT ID IN PROFILEPICS
 
-      $username = $_POST['username'];
+    $username = $_POST['username'];
 	  $summary = $_POST['summary'];
 	  $summary = htmlspecialchars($summary, ENT_QUOTES);
-	  $insert_sql = "UPDATE account SET username = '$username', summary= '$summary'";
-
+    $getuname = "SELECT username FROM account WHERE email = '$email'";
+    $getunamecon = mysqli_query($conn, $getuname);
+    $oldunameres = mysqli_fetch_assoc($getunamecon);
+    $olduname = $oldunameres['username'];
+    $unamecomm = "UPDATE comment SET username = '$username' WHERE username = '$olduname'";
+    $unamecomres = mysqli_query($conn, $unamecomm);
+	  $insert_sql = "UPDATE account SET username = '$username', summary= '$summary' WHERE email = '$email'";
       $insert = mysqli_query($conn,$insert_sql);
 	  if($insert){
 	    echo "default register success";
@@ -205,16 +417,15 @@
 
 	}
   ?>
+    <div class='boxcontainer'>
     <form action="editprofile.php" method="post" enctype="multipart/form-data" id="forms">
-	  <label for="username">Username</label>
-	  <input type="text" name="username" placeholder="Enter your username here..." autocomplete="off" required>
+	  <input class = 'textareas' type="text" name="username" placeholder="Enter your username here..." autocomplete="off" required>
 	  <br>
-	  <label for="summary">Summary: </label>
-	  <textarea name="summary" rows="3" cols="40" placeholder="Enter your summary here..."></textarea>
+	  <textarea class = 'textareas' name="summary" rows="3" cols="40" placeholder="Enter your summary here..." ></textarea>
 	  <br>
-      <input type="submit" name="next" value="Next">
+      <input type="submit" class="submitpost"name="next" value="Next">
 	</form>
-
+</div>
  <script>
 	//// IF USER HASNT LOGGED IN VALIDATION in functions.js///
 	check_login();
