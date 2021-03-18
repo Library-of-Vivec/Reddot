d5214a0ebaa4d4140a8b8f718821b4a5fc1ef1fc
armaroz <jensjovellano@gmail.com>
2021-03-18 22:46:29 +0800

    Update registration.php

--- Database/registration.php
+++ Database/registration.php
@@ -4,6 +4,7 @@
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
   <script src="https://apis.google.com/js/platform.js" async defer> </script>
   <script src="functions.js"></script>
+  <link rel="stylesheet" type="text/css" href="regstyle.css"/>
   </head>
   <body>
   <script>
@@ -14,7 +15,6 @@
   </script>
   <?php
     $email = $_COOKIE["email"];
-	echo $email;
 	$pos = strpos($email,"@");
 	$id = substr($email, 0, $pos);
 	include 'config.php';
@@ -23,12 +23,12 @@
 	{
 	  //temp_profilepics = pang store lang ng temp na pic nadedelete din after
 	  //profilepics = official folder ng profile pic
-	  
-	  //GET DEFAULT PICTURE 
+
+	  //GET DEFAULT PICTURE
 	  //RENAMING DEFAULT PICTURE TO STUDENT ID IN PROFILEPICS
 	  copy("default.png", "temp_profilepics/".$id.".png");
       copy("temp_profilepics/".$id.".png", "profilepics/".$id.".png");
-	  
+
       $username = $_POST['username'];
 	  $summary = $_POST['summary'];
 	  $summary = htmlspecialchars($summary, ENT_QUOTES);
@@ -40,31 +40,33 @@
 	    echo "default register success";
 		setcookie("reg", "yes");
 		setcookie("setup", "notcomplete");
-		$folder_path = "temp_profilepics"; 
-		$files = glob($folder_path.'/*');  
+		$folder_path = "temp_profilepics";
+		$files = glob($folder_path.'/*');
 		// Deleting all the files in the temp_profilepics
-		foreach($files as $file) { 
-		  if(is_file($file))  
-				unlink($file);  
-		} 
+		foreach($files as $file) {
+		  if(is_file($file))
+				unlink($file);
+		}
 	    echo "<script>location.replace(\"setup_pic.php\")</script>";
 	  }
 	  else{
         echo "Username already exists!";
-	  }  
+	  }
 
 	}
   ?>
+  <div class = "boxcontainer">
+  <div = "dispdiv">
     <form action="registration.php" method="post" enctype="multipart/form-data" id="forms">
 	  <label for="username">Username</label>
-	  <input type="text" name="username" placeholder="Enter your username here..." autocomplete="off" required>
+	  <input type="text" class = "textareas" name="username" placeholder="Enter your username here..." autocomplete="off" required>
 	  <br>
 	  <label for="summary">Summary: </label>
-	  <textarea name="summary" rows="3" cols="40" placeholder="Enter your summary here..."></textarea>
+	  <textarea name="summary" class = "textareas" rows="3" cols="40" placeholder="Enter your summary here..."></textarea>
 	  <br>
-      <input type="submit" name="next" value="Next">
+      <input class = "submitpost" type="submit" name="next" value="Next">
 	</form>
-
-
+</div>
+</div>
   </body>
  </html>
